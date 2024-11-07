d5b5cfab37d90e7c2cbd3bf3e2a0d9a57285b0c9
João Lobo <joao@lobo.is>
2024-11-07 15:39:21 +0000

    feat: project setup and initial server logic

---
+++ app/src/main/java/com/group15/kvserver/Server.java
@@ -0,0 +1,150 @@
+package com.group15.kvserver;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.Set;
+
+enum RequestType {
+    AuthRequest((short)0),
+    RegisterRequest((short)1),
+    PutRequest((short)2),
+    GetRequest((short)3),
+    MultiPutRequest((short)4),
+    MultiGetRequest((short)5);
+
+    private final short value;
+
+    RequestType(short value) {
+        this.value = value;
+    }
+
+    public short getValue() {
+        return value;
+    }
+}
+
+class ServerDatabase {
+    Map<String, byte[]> database;
+
+    // key: username, value: password
+    Map<String, String> users;
+
+    public ServerDatabase() {
+        this.database = new java.util.HashMap<>();
+    }
+}
+
+class ServerWorker implements Runnable {
+    private Socket socket;
+    private ServerDatabase database;
+
+    public ServerWorker(Socket socket, ServerDatabase database) {
+        this.socket = socket;
+        this.database = database;
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            boolean running = true;
+            while (running) {
+                short requestType = in.readShort();
+                RequestType r = RequestType.values()[requestType];
+                DataOutputStream stream = handleRequest(r, in);
+
+                if (stream != null) {
+                    stream.flush();
+                }
+            }
+
+            socket.shutdownInput();
+            socket.shutdownOutput();
+            socket.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DataOutputStream handleRequest(RequestType requestType, DataInputStream in){
+        // TODO: Auth -> Mário
+        // TODO: Puts -> Lobo
+        // TODO: Gets -> Rita
+
+        switch (requestType) {
+            case AuthRequest:
+                return handleAuthRequest(in);
+            case RegisterRequest:
+                // return handleRegisterRequest(in);
+            case PutRequest:
+                // handlePutRequest(in);
+            case GetRequest:
+                // return handleGetRequest(in);
+            case MultiPutRequest:
+                // handleMultiPutRequest(in);
+            case MultiGetRequest:
+                // return handleMultiGetRequest(in);
+            default:
+                return null;
+        }
+    }
+
+    private DataOutputStream handleAuthRequest(DataInputStream in) {
+        try {
+            String username = in.readUTF();
+            String password = in.readUTF();
+
+            if (database.users.containsKey(username) && database.users.get(username).equals(password)) {
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                out.writeBoolean(true);
+                return out;
+            }
+            else {
+                return null;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    void put(String key, byte[] value) {
+        database.database.put(key, value);
+    }
+
+    byte[] get(String key) {
+        return database.database.get(key);
+    }
+
+    void multiPut(Map<String, byte[]> pairs) {
+        database.database.putAll(pairs);
+    }
+
+    Map<String, byte[]> multiGet(Set<String> keys) {
+        return database.database.entrySet().stream()
+            .filter(entry -> keys.contains(entry.getKey()))
+            .collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerDatabase database = new ServerDatabase(); 
+        ServerSocket serverSocket = new ServerSocket(12345);
+
+        boolean running = true;
+        while (running) {
+            Socket socket = serverSocket.accept();
+            Thread worker = new Thread(new ServerWorker(socket, database));
+            worker.start();
+        }
+    }
+}
