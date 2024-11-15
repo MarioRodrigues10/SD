649324d02c6a2c60a4feb87fdfa259cbdf35cc5a
João Lobo <30907944+joaodiaslobo@users.noreply.github.com>
2024-11-15 17:00:24 +0000

    feat: add sharding (#5)

--- app/src/main/java/com/group15/kvserver/Server.java
+++ app/src/main/java/com/group15/kvserver/Server.java
@@ -4,7 +4,9 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.Set;
 
@@ -28,21 +30,48 @@ enum RequestType {
 }
 
 class ServerDatabase {
-    Map<String, byte[]> database;
+    int databaseShardsCount;
+    int usersShardsCount;
 
-    // key: username, value: password
-    Map<String, String> users;
+    List<Map<String, byte[]>> databaseShards;
+    List<Map<String, String>> usersShards;
 
-    public ServerDatabase() {
-        this.database = new java.util.HashMap<>();
-        this.users = new java.util.HashMap<>();
+    List<ReentrantReadWriteLock> databaseLocks;
+    List<ReentrantLock> usersLocks;
+
+    public ServerDatabase(int maxClients, int databaseShardsCount, int usersShardsCount) {
+        this.databaseShardsCount = databaseShardsCount;
+        this.usersShardsCount = usersShardsCount;
+        
+        this.databaseShards = new java.util.ArrayList<>();
+        this.usersShards = new java.util.ArrayList<>();
+
+        this.databaseLocks = new java.util.ArrayList<>();
+        this.usersLocks = new java.util.ArrayList<>();
+
+        for (int i = 0; i < databaseShardsCount; i++) {
+            this.databaseShards.add(new HashMap<>());
+            this.databaseLocks.add(new ReentrantReadWriteLock());
+        }
+
+        for (int i = 0; i < usersShardsCount; i++) {
+            this.usersShards.add(new HashMap<>());
+            this.usersLocks.add(new ReentrantLock());
+        }
+    }
+
+    public int getDatabaseShardIndex(String key) {
+        return Math.abs(key.hashCode()) % databaseShardsCount;
+    }
+
+    public int getUsersShardIndex(String key) {
+        return Math.abs(key.hashCode()) % usersShardsCount;
     }
 }
 
 class ServerWorker implements Runnable {
     private Socket socket;
     private ServerDatabase database;
-    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public ServerWorker(Socket socket, ServerDatabase database) {
         this.socket = socket;
@@ -86,170 +115,156 @@ class ServerWorker implements Runnable {
     }
 
     public DataOutputStream handleRequest(RequestType requestType, DataInputStream in){
-
-        switch (requestType) {
-            case AuthRequest:
-                return handleAuthRequest(in);
-            case RegisterRequest:
-                return handleRegisterRequest(in);
-            case PutRequest:
-                return handlePutRequest(in);
-            case GetRequest:
-                return handleGetRequest(in);
-            case MultiPutRequest:
-                return handleMultiPutRequest(in);
-            case MultiGetRequest:
-                return handleMultiGetRequest(in);
-            default:
-                return null;
+        try {
+            switch (requestType) {
+                case AuthRequest:
+                    return handleAuthRequest(in);
+                case RegisterRequest:
+                    return handleRegisterRequest(in);
+                case PutRequest:
+                    return handlePutRequest(in);
+                case GetRequest:
+                    return handleGetRequest(in);
+                case MultiPutRequest:
+                    return handleMultiPutRequest(in);
+                case MultiGetRequest:
+                    return handleMultiGetRequest(in);
+                default:
+                    return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
-    private DataOutputStream handleAuthRequest(DataInputStream in) {
-        lock.readLock().lock(); 
+    private DataOutputStream handleAuthRequest(DataInputStream in) throws IOException {
+        String username = in.readUTF();
+        String password = in.readUTF();
+        int userShardIndex = database.getUsersShardIndex(username);
+        database.usersLocks.get(userShardIndex).lock();
         try {
-            String username = in.readUTF();
-            String password = in.readUTF();
-
-            if (database.users.containsKey(username) && database.users.get(username).equals(password)) {
+            Map<String, String> currentShard = database.usersShards.get(userShardIndex);
+            if (currentShard.containsKey(username) && currentShard.get(username).equals(password)) {
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 out.writeBoolean(true);
                 return out;
             } else {
                 return null;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            lock.readLock().unlock(); 
+        } 
+        finally {
+            database.usersLocks.get(userShardIndex).unlock();
         }
     }
 
-    private DataOutputStream handleRegisterRequest(DataInputStream in) {
-        lock.writeLock().lock();
+    private DataOutputStream handleRegisterRequest(DataInputStream in) throws IOException {
+        String username = in.readUTF();
+        String password = in.readUTF();
+        int userShardIndex = database.getUsersShardIndex(username);
+        database.usersLocks.get(userShardIndex).lock();
         try {
-            String username = in.readUTF();
-            String password = in.readUTF();
-
-            if (database.users.containsKey(username)) {
+            Map<String, String> currentShard = database.usersShards.get(userShardIndex);
+            if (currentShard.containsKey(username)) {
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 out.writeBoolean(false);
                 return out;
             } else {
-                database.users.put(username, password);
+                currentShard.put(username, password);
 
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 out.writeBoolean(true);
                 return out;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
         } finally {
-            lock.writeLock().unlock();
+            database.usersLocks.get(userShardIndex).unlock();
         }
     }
 
-    private DataOutputStream handlePutRequest(DataInputStream in){
-        try {
+    private DataOutputStream handlePutRequest(DataInputStream in) throws IOException{
+        String key = in.readUTF();
+        int valueLength = in.readInt();
+        byte[] value = new byte[valueLength];
+        in.readFully(value);
+
+        put(key, value);
+
+        return null;
+    }
+
+    private DataOutputStream handleGetRequest(DataInputStream in) throws IOException{
+        // KEY
+        String key = in.readUTF();
+        byte[] value = get(key);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        // VALUE SIZE | VALUE
+        out.writeInt(value.length);
+        out.write(value);
+        return out;
+    }
+
+    private DataOutputStream handleMultiPutRequest(DataInputStream in) throws IOException {
+        // N PAIRS | KEY | VALUE LENGTH | VALUE | KEY | VALUE LENGTH | VALUE | ...
+        int numberOfPairs = in.readInt();
+        Map<String, byte[]> pairs = new java.util.HashMap<>();
+
+        for (int i = 0; i < numberOfPairs; i++) {
             String key = in.readUTF();
             int valueLength = in.readInt();
             byte[] value = new byte[valueLength];
             in.readFully(value);
-
-            put(key, value);
-
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            pairs.put(key, value);
         }
+
+        multiPut(pairs);
+
+        return null;
     }
 
-    private DataOutputStream handleGetRequest(DataInputStream in){
-        try {
-            // KEY
+    private DataOutputStream handleMultiGetRequest(DataInputStream in) throws IOException {
+        // N KEYS | KEY | ...
+        int numberOfKeys = in.readInt();
+        Set<String> keys = new java.util.HashSet<>();
+        for (int i = 0; i < numberOfKeys; i++) {
             String key = in.readUTF();
-            byte[] value = get(key);
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            keys.add(key);
+        }
+        Map<String, byte[]> pairs = multiGet(keys);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            // VALUE SIZE | VALUE
+        // N PAIRS | KEY | VALUE LENGTH | VALUE ..
+        out.writeInt(numberOfKeys);
+        for (Map.Entry<String, byte[]> pair : pairs.entrySet()) {
+            out.writeUTF(pair.getKey());
+            byte[] value = pair.getValue();
             out.writeInt(value.length);
             out.write(value);
-            return out;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
         }
-    }
 
-    private DataOutputStream handleMultiPutRequest(DataInputStream in) {
-        try {
-            // N PAIRS | KEY | VALUE LENGTH | VALUE | KEY | VALUE LENGTH | VALUE | ...
-            int numberOfPairs = in.readInt();
-            Map<String, byte[]> pairs = new java.util.HashMap<>();
-    
-            for (int i = 0; i < numberOfPairs; i++) {
-                String key = in.readUTF();
-                int valueLength = in.readInt();
-                byte[] value = new byte[valueLength];
-                in.readFully(value);
-                pairs.put(key, value);
-            }
-    
-            multiPut(pairs);
-
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private DataOutputStream handleMultiGetRequest(DataInputStream in) {
-        try {
-            // N KEYS | KEY | ...
-            int numberOfKeys = in.readInt();
-            Set<String> keys = new java.util.HashSet<>();
-            for (int i = 0; i < numberOfKeys; i++) {
-                String key = in.readUTF();
-                keys.add(key);
-            }
-            Map<String, byte[]> pairs = multiGet(keys);
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            // N PAIRS | KEY | VALUE LENGTH | VALUE ..
-            out.writeInt(numberOfKeys);
-            for (Map.Entry<String, byte[]> pair : pairs.entrySet()) {
-                out.writeUTF(pair.getKey());
-                byte[] value = pair.getValue();
-                out.writeInt(value.length);
-                out.write(value);
-            }
-
-            return out;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return out;
     }
 
     private void put(String key, byte[] value) {
-        lock.writeLock().lock();
+
+        int shardIndex = database.getDatabaseShardIndex(key);
+        database.databaseLocks.get(shardIndex).writeLock().lock();
         try {
-            database.database.put(key, value);
+            Map<String, byte[]> currentShard = database.databaseShards.get(shardIndex);
+            currentShard.put(key, value);
         } finally {
-            lock.writeLock().unlock();
+            database.databaseLocks.get(shardIndex).writeLock().unlock();
         }
     }
 
     private byte[] get(String key) {
-        lock.readLock().lock();
+        int shardIndex = database.getDatabaseShardIndex(key);
+        database.databaseLocks.get(shardIndex).readLock().lock();
         try {
-            return database.database.get(key);
+            Map<String, byte[]> currentShard = database.databaseShards.get(shardIndex);
+            return currentShard.get(key);
         } finally {
-            lock.readLock().unlock();
+            database.databaseLocks.get(shardIndex).readLock().unlock();
         }
     }
     
@@ -273,7 +288,23 @@ class ServerWorker implements Runnable {
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerDatabase database = new ServerDatabase(); 
+        List<Integer> arguments = new java.util.ArrayList<>();
+
+        if(args.length == 3) {
+            for(String arg : args) {
+                try {
+                    arguments.add(Integer.parseInt(arg));
+                } catch (NumberFormatException e) {
+                    System.out.println("Usage: java Server <max-clients> <database-shards> <user-shards>");
+                    return;
+                }
+            }
+        } else {
+            System.out.println("Usage: java Server <max-clients> <database-shards> <user-shards>");
+            return;
+        }
+
+        ServerDatabase database = new ServerDatabase(arguments.get(0), arguments.get(1), arguments.get(2)); 
         ServerSocket serverSocket = new ServerSocket(12345);
 
         boolean running = true;
